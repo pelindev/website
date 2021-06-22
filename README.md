66fbe76a60079e3de7b0e2f2eb08747321349588
Nikita Pelin <pelin.dev@gmail.com>
2021-06-22 17:37:34 +0300

    added README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Website for Dev Feature test
+Run:
+```
+docker-compose up
+```
+url:
+
+http://localhost:9000
--- www/app/qiwi.php
+++ www/app/qiwi.php
@@ -1,10 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+
 const SECRET_KEY = 'eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6Im04aTN6NC0wMCIsInVzZXJfaWQiOiI3OTk5NjkyMjcwOSIsInNlY3JldCI6IjhmN2RiZjcxZDY0NTUwMmZiOTE3ZjAzZGY4ODViMWJjZjdjMjJkZWFkOWM1Zjc0ZmQyNGUxZmY0YTFkY2E2NzEifX0=';
-
 $billPayments = new Qiwi\Api\BillPayments(SECRET_KEY);
-
 $billId = 'cc961e8d-d4d6-4f02-b737-2297e51fb48e' . mt_rand(0,999) . '-' . mt_rand(0,999);
+
 $fields = [
   'amount' => $_POST['value'],
   'currency' => 'RUB',
@@ -14,10 +14,5 @@ $fields = [
 /** @var \Qiwi\Api\BillPayments $billPayments */
 $response = $billPayments->createBill($billId, $fields);
 $location = $response['payUrl'];
-
-
 header("Location: $location");
-
-// print_r($response);
-
 ?>
